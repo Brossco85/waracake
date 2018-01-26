3de56db6db4b2d64565eae5cd668ac572a1d990d
Bobby <Bobby1985uk@gmail.com>
2018-01-26 23:20:34 +0000

    added cake model

--- server/models/cake.js
+++ server/models/cake.js
@@ -1,1 +1,30 @@
 const mongoose = require('mongoose');
+
+const Cake = mongoose.model('Cake', {
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  comment: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  yumFactor: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  }
+});
+
+module.exports = {Cake};
