b72248df3eeaab2025f6f5c64fbee8dad76e163c
Bobby <Bobby1985uk@gmail.com>
2018-01-27 20:06:37 +0000

    added CakesIndex Component and react router dependancy

---
+++ client/src/components/CakesIndex.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class CakesIndex extends Component {
+  render() {
+    return  (
+      <div>
+      Cakes Index
+      </div>
+      );
+  }
+}
+
+export default CakesIndex;
