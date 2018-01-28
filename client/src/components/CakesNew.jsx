a76e04b5529c310ec406cb42b0c90443e6398b6e
Bobby <Bobby1985uk@gmail.com>
2018-01-28 16:37:47 +0000

    added custom redux form validation

--- client/src/components/CakesNew.jsx
+++ client/src/components/CakesNew.jsx
@@ -26,6 +26,29 @@ class CakesNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if(!values.name) {
+    errors.name = "Please enter a valid name";
+  }
+
+  if(!values.comment) {
+    errors.comment = "Please enter a Comment to go with your cake";
+  }
+
+  if(!values.imageurl) {
+    errors.imageurl = "Please include an image of your cake";
+  }
+
+  if(!values.yumfactor < 0 || !values.yumfactor > 5) {
+    errors.yumfactor = "Please enter a Yum Factor Between 1 and 5";
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'CakesNewForm'
 })(CakesNew);
