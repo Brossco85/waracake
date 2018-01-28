cc67a9905c336d10dc68e525bad1ec1511258092
Bobby <Bobby1985uk@gmail.com>
2018-01-28 10:23:44 +0000

    added actions folder, redux, redux-promise and axios packages

---
+++ client/src/actions/index.js
@@ -0,0 +1,7 @@
+export const FETCH_CAKES = 'fetch_cakes';
+
+export function fetchPosts() {
+  return {
+    type: FETCH_CAKES
+  };
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 // import App from './App';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import registerServiceWorker from './registerServiceWorker';
 
