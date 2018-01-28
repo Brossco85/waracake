2e84fe07f4399d40848cd94064ebe78e0bb9634f
Bobby <Bobby1985uk@gmail.com>
2018-01-28 18:36:06 +0000

    added Cakes Show component and route

---
+++ client/src/components/CakesShow.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CakesShow extends Component {
+  render() {
+    return (
+      <div>Cake Show!</div>
+   );
+  };
+}
+
+export default CakesShow
--- client/src/index.js
+++ client/src/index.js
@@ -12,19 +12,21 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import CakesIndex from './components/CakesIndex';
 import CakesNew from './components/CakesNew';
+import CakesShow from './components/CakesShow';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-  <BrowserRouter>
-  <div>
-  <Switch>
-  <Route path='/cakes/new' component={CakesNew} />
-  <Route path='/' component={CakesIndex} />
-  </Switch>
-  </div>
-  </BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path='/cakes/new' component={CakesNew} />
+          <Route path='/cakes/:id' component={CakesShow} />
+          <Route path='/' component={CakesIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
   );
