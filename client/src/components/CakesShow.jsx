ebf26e9458f52398ed8bc747425221b66113cca8
Bobby <Bobby1985uk@gmail.com>
2018-01-28 19:02:40 +0000

    added map state to props function for Cakes Show

--- client/src/components/CakesShow.jsx
+++ client/src/components/CakesShow.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCake } from '../actions';
 
 class CakesShow extends Component {
+  
+  componentDidMount() {
+    const { id } = this.props.match.params.id;
+    this.props.fetchCake(id);
+  }
+
   render() {
     return (
       <div>Cake Show!</div>
    );
-  };
+  }
 }
 
-export default CakesShow;
+function mapStateToProps({ cakes }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchCake })(CakesShow);
