a1a5ba4eee135fccae287d353222c91782cd37bc
Bobby <Bobby1985uk@gmail.com>
2018-01-28 11:55:42 +0000

    fixed bug in cakes reducer to access cakes element of array, added renderCakes function to print list of cake names to screen

--- client/src/components/CakesIndex.jsx
+++ client/src/components/CakesIndex.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchCakes } from '../actions';
@@ -8,13 +9,30 @@ class CakesIndex extends Component {
     this.props.fetchCakes();
   }
 
+  renderCakes() {
+    return _.map(this.props.cakes, cake => {
+      return (
+        <li key={cake._id}>
+        {cake.name}
+        </li>
+        );
+    });
+  }
+
   render() {
     return  (
       <div>
-      Cakes Index
+      <h3>Waracakes List</h3>
+      <ul>
+      {this.renderCakes()}
+      </ul>
       </div>
       );
   }
 }
 
-export default connect(null, { fetchCakes })(CakesIndex);
+function mapStateToProps(state) {
+  return { cakes: state.cakes };
+}
+
+export default connect(mapStateToProps, { fetchCakes })(CakesIndex);
--- client/src/reducers/reducer_cakes.js
+++ client/src/reducers/reducer_cakes.js
@@ -4,7 +4,7 @@ import { FETCH_CAKES } from '../actions';
 export default function(state = {}, action) {
   switch(action.type) {
     case FETCH_CAKES:
-    return _.mapKeys(action.payload.data, 'id');
+    return _.mapKeys(action.payload.data.cakes, '_id');
     default:
     return state;
   }
