5da5ae910d0a220b00f0e4dd3e3dffeb71d1522d
Bobby Ross <bobby1985uk@gmail.com>
2018-01-30 15:51:53 +0000

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,6 @@ if (env === 'development') {
   process.env.PORT = 3001;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/WaracakeApp';
 } else if (env === 'test') {
-  process.env.PORT = 3001;
+  process.env.PORT = 3002;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/WaracakeAppTest';
 }
