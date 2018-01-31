4cd49cf8bafa1c4415e95fdf6942ff75fec25dc7
Bobby <Bobby1985uk@gmail.com>
2018-01-31 14:34:28 +0000

    fixed naming on cakesReducer file and in reducer index js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from "redux-form";
-import CakesReducer from './reducer_cakes';
+import CakesReducer from './reducerCakes';
 
 const rootReducer = combineReducers({
   cakes: CakesReducer,
